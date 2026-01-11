63b7b9cca7cceabeb5422cb98a0fe77ad64edfbc
Programmer <aceraspire315510@gmail.com>
2026-01-11 22:22:08 +0500

    version 2.0

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 # Application definition
 
