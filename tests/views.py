0a6c1a59eb57ba1258d49bd530cd9aa66077aab0
Programmer <aceraspire315510@gmail.com>
2026-01-21 00:13:23 +0500

    version 4.0

--- tests/views.py
+++ tests/views.py
@@ -16,10 +16,6 @@ def test_list(request, subject_id):
     test_count = math.ceil(questions.count() / PAGE_SIZE)
     tests = range(1, test_count + 1)
 
-    # session reset
-    request.session['q_index'] = 0
-    request.session['score'] = 0
-
     return render(request, 'tests/test_list.html', {
         'subject': subject,
         'tests': tests,
@@ -27,12 +23,17 @@ def test_list(request, subject_id):
     })
 
 def quiz(request, subject_id, test_no):
-    q_index = request.session.get('q_index', 0)
+    # session kalitlari test_no bo'yicha
+    q_index_key = f'q_index_{subject_id}_{test_no}'
+    score_key = f'score_{subject_id}_{test_no}'
+
+    q_index = request.session.get(q_index_key, 0)
+
     subject = Subject.objects.get(id=subject_id)
     questions = Question.objects.filter(subject=subject)
     start = (test_no - 1) * PAGE_SIZE
     end = start + PAGE_SIZE
-    group = questions[start:end]
+    group = list(questions[start:end])
 
     if q_index >= len(group):
         return redirect('result', subject_id=subject.id, test_no=test_no)
@@ -51,36 +52,42 @@ def quiz(request, subject_id, test_no):
 
 def submit_answer(request, subject_id, test_no):
     if request.method == 'POST':
-        answer_id = request.POST.get('answer')
-        q_index = request.session.get('q_index', 0)
+        q_index_key = f'q_index_{subject_id}_{test_no}'
+        score_key = f'score_{subject_id}_{test_no}'
+
+        q_index = request.session.get(q_index_key, 0)
+        answer_id = request.POST.get('answer_id')  # JS orqali yuboriladi
+
         subject = Subject.objects.get(id=subject_id)
         questions = Question.objects.filter(subject=subject)
         start = (test_no - 1) * PAGE_SIZE
         end = start + PAGE_SIZE
-        group = questions[start:end]
+        group = list(questions[start:end])
 
         if answer_id:
             ans = Answer.objects.get(id=answer_id)
             if ans.is_correct:
-                request.session['score'] = request.session.get('score', 0) + 1
+                request.session[score_key] = request.session.get(score_key, 0) + 1
 
-        request.session['q_index'] = q_index + 1
+        request.session[q_index_key] = q_index + 1
 
     return redirect('quiz', subject_id=subject_id, test_no=test_no)
 
 def result(request, subject_id, test_no):
+    score_key = f'score_{subject_id}_{test_no}'
+
     subject = Subject.objects.get(id=subject_id)
     questions = Question.objects.filter(subject=subject)
     start = (test_no - 1) * PAGE_SIZE
     end = start + PAGE_SIZE
-    group = questions[start:end]
+    group = list(questions[start:end])
 
-    score = request.session.get('score', 0)
+    score = request.session.get(score_key, 0)
     total = len(group)
 
-    # reset
-    request.session['q_index'] = 0
-    request.session['score'] = 0
+    # reset faqat test tugaganda
+    request.session[f'q_index_{subject_id}_{test_no}'] = 0
+    request.session[score_key] = 0
 
     return render(request, 'tests/result.html', {
         'score': score,
